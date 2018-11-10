e4f75aca2aa8d4679dac0892f1909dc9956745d1
Louis Perrochon <perrochon@gmail.com>
2018-11-10 09:10:06 -0800

    Initial Commit. Inspiration links only.

---
+++ gblib/src/main/java/com/zwsi/gblib/GBShip.java
@@ -0,0 +1,11 @@
+// Copyright 2018 Louis Perrochon. All rights reserved
+//
+// // Inspiration
+// https://github.com/kaladron/galactic-bloodshed/blob/master/data/exam.dat
+// https://github.com/kaladron/galactic-bloodshed/blob/master/data/ship.dat
+
+package com.zwsi.gblib;
+
+public class GBShip {
+}
+
