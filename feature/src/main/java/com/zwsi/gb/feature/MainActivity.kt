2c17e67e756508b39269f6217248c060904d0714
Louis Perrochon <perrochon@gmail.com>
2018-11-13 19:41:50 -0800

    Version to 4 digits.

--- feature/src/main/java/com/zwsi/gb/feature/MainActivity.kt
+++ feature/src/main/java/com/zwsi/gb/feature/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var version = findViewById<TextView>(R.id.version)
-        version.setText("0.0.40") // for now: 0.0.~ #commits...
+        version.setText("0.0.0.40") // for now: 0.0.0.~ #commits...
     }
 
     /** Called when the user taps the Create button */
