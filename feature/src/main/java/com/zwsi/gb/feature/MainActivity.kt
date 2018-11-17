642260949381f0f79a7f555be072168f2d9a2dd1
Louis Perrochon <perrochon@gmail.com>
2018-11-17 15:10:56 -0800

    Xenos with transparent background

--- feature/src/main/java/com/zwsi/gb/feature/MainActivity.kt
+++ feature/src/main/java/com/zwsi/gb/feature/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var version = findViewById<TextView>(R.id.version)
-        version.setText("0.0.0.74") // for now: 0.0.0.~ #commits...
+        version.setText("0.0.0.87") // for now: 0.0.0.~ #commits...
 
 
         if (GBController.universe == null) {
