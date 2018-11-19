0d22d08d174ad2226af0637d7804b4dbcbff121a
Louis Perrochon <perrochon@gmail.com>
2018-11-19 14:14:03 -0800

    .123 release

--- feature/src/main/java/com/zwsi/gb/feature/MainActivity.kt
+++ feature/src/main/java/com/zwsi/gb/feature/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val version = findViewById<TextView>(R.id.version)
-        version.setText("0.0.0.97") // for now: 0.0.0.~ #commits...
+        version.setText("0.0.0.123") // for now: 0.0.0.~ #commits...
 
 
         if (GBController.universe == null) {
