a524042388402cd11bacd841897d9ba187f10fc9
Louis Perrochon <perrochon@gmail.com>
2018-11-24 19:19:43 -0800

    Hint looks improvement

--- feature/src/main/java/com/zwsi/gb/feature/ShipsSlideActivity.kt
+++ feature/src/main/java/com/zwsi/gb/feature/ShipsSlideActivity.kt
@@ -27,9 +27,9 @@ class ShipsSlideActivity : AppCompatActivity() {
             hintText.visibility = (TextView.VISIBLE)
             hintText.setText(
                 "You haven't built any ships yet.\n\n " +
-                        "Start making ships by creating a Factory on your home planet" +
-                        "That Factory will be your first ship. Then order the Factory to make other ships." +
-                        "Remember that you have to give the order to build the factory first, then click on" +
+                        "Start making ships by creating a Factory on your home planet " +
+                        "That Factory will be your first ship. Then order the Factory to make other ships. " +
+                        "Remember that you have to give the order to build the factory first, then click on " +
                         "[Do] so your minions can execute your orders."
             )
 
