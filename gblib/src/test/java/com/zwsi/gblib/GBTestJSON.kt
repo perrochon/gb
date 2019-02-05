7521c3ca0a03fc92cb229eabb2de0e674fee0ab0
Louis Perrochon <perrochon@gmail.com>
2019-02-04 18:29:19 -0800

    Moving around test cases

--- gblib/src/test/java/com/zwsi/gblib/GBTestJSON.kt
+++ gblib/src/test/java/com/zwsi/gblib/GBTestJSON.kt
@@ -23,33 +23,6 @@ class GBTestJSON {
 
         val moshi = Moshi.Builder().build()
 
-        println("Testing GBLocation")
-        val loc1 = GBLocation(100f, 100f)
-        println("  GBLocation in: " + loc1)
-        val jsonAdapter1 = moshi.adapter<GBLocation>(GBLocation::class.java)
-        val json1 = jsonAdapter1.toJson(loc1)
-        println("  JSON string: " + json1)
-        val loc2 = jsonAdapter1.fromJson(json1)
-        println("  GBLocation out:  " + loc2)
-        assert(loc1 == loc2)
-
-        println("\nTesting List")
-        val list1: MutableList<GBLocation> = Collections.synchronizedList(arrayListOf<GBLocation>())
-        list1.add(GBLocation(100f, 100f))
-        list1.add(GBLocation(1000f, 1000f))
-        list1.add(GBLocation(u.allPlanets[1], 1, 1))
-        list1.add(GBLocation(u.allPlanets[1], 1f, 1f))
-        list1.add(GBLocation(u.allStars[1], 10f, 1f))
-        println("  List<GBLocation> in: " + list1)
-
-        val locListType = Types.newParameterizedType(List::class.java, GBLocation::class.java)
-        val jsonAdapter2: JsonAdapter<List<GBLocation>> = moshi.adapter(locListType)
-        val json2 = jsonAdapter2.toJson(list1)
-        println("  JSON string: " + json2)
-        val list2 = jsonAdapter2.fromJson(json2)
-        println("  List<GBLocation> out:  " + list2)
-        assert(list1 == list2)
-
         println("\n Testing GBRace")
         val race1 = GBRace(0, 0, u.allPlanets[0].uid)
         println(race1)
@@ -60,10 +33,8 @@ class GBTestJSON {
         println(race2)
         assert(race1 == race2)
 
-        val raceMap = mapOf(0 to race1)
-
-        println("\nTesting Location, List, Race combined")
-        val gameInfo1 = GBSavedGame(loc1, list1, race1, u.allRaces)
+        println("\nTesting GBSavedGame")
+        val gameInfo1 = GBSavedGame(null, null, race1, u.allRaces)
         println("  GBSavedGame in: " + gameInfo1)
         val jsonAdapter3: JsonAdapter<GBSavedGame> = moshi.adapter(GBSavedGame::class.java)
         val json3 = jsonAdapter3.toJson(gameInfo1)
