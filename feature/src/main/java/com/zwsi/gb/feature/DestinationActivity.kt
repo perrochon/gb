68596c435fcc971fe13f47a662ddce53d35b9fd4
Louis Perrochon <perrochon@gmail.com>
2019-03-13 10:22:17 -0700

    Replaced Destinatin Spinner with Destination Selection Activity

--- feature/src/main/java/com/zwsi/gb/feature/DestinationActivity.kt
+++ feature/src/main/java/com/zwsi/gb/feature/DestinationActivity.kt
@@ -1,18 +1,18 @@
 package com.zwsi.gb.feature
 
+import android.graphics.LightingColorFilter
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
 import com.zwsi.gb.feature.GBViewModel.Companion.vm
-import com.zwsi.gblib.distance
-import android.graphics.drawable.Drawable
-import android.util.AndroidException
-import android.widget.RadioButton
-import android.widget.RadioGroup
-
-
+import com.zwsi.gblib.*
+import com.zwsi.gblib.GBLocation.Companion.LANDED
+import com.zwsi.gblib.GBLocation.Companion.ORBIT
+import com.zwsi.gblib.GBLocation.Companion.PATROL
 
 
 class DestinationActivity : AppCompatActivity() {
@@ -27,29 +27,46 @@ class DestinationActivity : AppCompatActivity() {
 
         val linearLayout = findViewById<LinearLayout>(R.id.Destinations)
 
-        val rg = RadioGroup(this) //create the RadioGroup
-        rg.orientation = RadioGroup.VERTICAL
+        val lp = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        lp.setMargins(8, 8, 8, 8)
 
-        var destination : RadioButton
+        val destinationsList = arrayListOf<Button>()
 
         val shipStar = ship.getStar() // FIXME This likely returns a server object.... here and elsewhere...
         val shipPlanet = ship.loc.getPlanet()
-        val planetDrawable = getDrawable(R.drawable.planet) // FIXME Get this from bitmap factory?
+        val planetDrawable = getDrawable(R.drawable.planet)!! // FIXME Get this from bitmap factory?
+        val starDrawable = getDrawable(R.drawable.star)!! // FIXME Get this from bitmap factory?
+
+        var uidSelectedPlanet = -1
+        var uidSelectedStar  = -1
+
+        if (ship.dest != null) {
+            if (ship.dest!!.level == ORBIT || ship.dest!!.level == LANDED) {
+                uidSelectedPlanet = ship.dest!!.getPlanet()!!.uid
+            } else if (ship.dest!!.level == PATROL) {
+                uidSelectedStar = ship.dest!!.getStar()!!.uid
+            }
+        }
 
         if (shipStar != null) {
             for (p in shipStar.starUidPlanets.map { vm.planet(it) }) {
-                destination = RadioButton(this)
-                destination.text = "Planet ${p.name}"
-                destination.height = 150
-                if (shipPlanet != null && p.uid == shipPlanet.uid) {
-//                    destination.text("(this planet)") // FIXME This crashes
+
+                val button = getButton(planetDrawable, "Planet ${p.name}")
+                if (uidSelectedPlanet == p.uid) {
+                    button.background.colorFilter = LightingColorFilter(0x55555, 0x774400)
                 }
-                destination.setCompoundDrawablesWithIntrinsicBounds(planetDrawable, null, null, null)
-                destination.setOnClickListener(View.OnClickListener {
-                    destinationClicked(it)
+                button.setOnClickListener(View.OnClickListener {
+                    handleClick(it, destinationsList)
+                    uidSelectedPlanet = p.uid
+                    uidSelectedStar = -1
                 })
-                rg.addView(destination)
+
+
+
+                linearLayout.addView(button, lp)
+                destinationsList.add(button)
             }
+
         }
 
         val sortedStars =
@@ -57,27 +74,81 @@ class DestinationActivity : AppCompatActivity() {
                 s.loc.getLoc().distance(ship.loc.getLoc())
             }.take(5)
 
-        val starDrawable = getDrawable(R.drawable.star) // FIXME Get this from bitmap factory?
         for ((_, star) in sortedStars) {
-            destination = RadioButton(this)
-            destination.text = "System ${star.name}"
-            destination.height = 150
-            if (shipStar != null && star.uid == shipStar.uid) {
-//                destination.append("(this system)") // FIXME This crashes
+
+            val button = getButton(starDrawable, "System ${star.name}")
+            if (uidSelectedStar == star.uid) {
+                button.background.colorFilter = LightingColorFilter(0x55555, 0x774400)
             }
-            destination.setCompoundDrawablesWithIntrinsicBounds(starDrawable, null, null, null)
-            destination.setOnClickListener(View.OnClickListener {
-                destinationClicked(it)
+            button.setOnClickListener(View.OnClickListener {
+                handleClick(it, destinationsList)
+                uidSelectedPlanet = -1
+                uidSelectedStar = star.uid
+
             })
-            rg.addView(destination)
+
+            linearLayout.addView(button, lp)
+            destinationsList.add(button)
         }
 
-        linearLayout.addView(rg)
+        val buttonOK = findViewById<Button>(R.id.okDestination)
+
+        buttonOK.setOnClickListener(View.OnClickListener {
+            if (uidSelectedPlanet != -1) {
+
+                val planet = vm.planet(uidSelectedPlanet)
+
+                if (ship.idxtype == GBData.POD || ship.idxtype == GBData.SHUTTLE) {
+                    GBController.flyShipLanded(ship.uid, planet.uid) // update server side
+                    ship.dest = GBLocation(planet, 0, 0) // update vm
+                } else {
+                    GBController.flyShipPlanetOrbit(ship.uid, planet.uid)// update server side
+                    ship.dest = GBLocation(planet, GBData.PlanetOrbit, 0f) // update vm
+                }
+
+            } else if (uidSelectedStar != -1) {
+                val star = vm.star(uidSelectedStar)
+                val patrolPoint: GBPatrolPoint
+                if (GBData.rand.nextBoolean()) {
+                    patrolPoint = vm.patrolPoint(star.starUidPatrolPoints.first())
+                } else {
+                    patrolPoint = vm.patrolPoint(star.starUidPatrolPoints.drop(1).first())
+                }
+                GBController.flyShipStarPatrol(ship.uid, patrolPoint.uid)// update server side
+                ship.dest = GBLocation(patrolPoint, 0f, 0f) // update vm // FIXME select patrol point
+            }
+            GlobalStuff.checkDo(it)
+            finish()
+        })
+
+        val buttonCancel = findViewById<Button>(R.id.cancelDestination)
+
+        buttonCancel.setOnClickListener(View.OnClickListener {
+            finish()
+        })
 
     }
 
-    private fun destinationClicked(button: View) {
+    private fun getButton(drawable: Drawable, text: String): Button {
 
+        val button = Button(this)
+        button.text = text
+        button.height = 140
+        button.setBackgroundResource(R.drawable.border_message)
+        button.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
+
+        return button
+    }
+
+    private fun handleClick(
+        button: View, destinationsList: List<Button>
+    ) {
+
+        var i = 0
+        for (b in destinationsList) {
+            b.background.colorFilter = LightingColorFilter(0, 0)
+        }
+        button.background.colorFilter = LightingColorFilter(0x55555, 0x774400)
 
     }
 
--- feature/src/main/java/com/zwsi/gb/feature/ShipFragment.kt
+++ feature/src/main/java/com/zwsi/gb/feature/ShipFragment.kt
@@ -9,13 +9,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import com.zwsi.gb.feature.GBViewModel.Companion.uidActivePlayer
 import com.zwsi.gb.feature.GBViewModel.Companion.vm
-import com.zwsi.gblib.GBController.Companion.flyShipLanded
-import com.zwsi.gblib.GBController.Companion.flyShipPlanetOrbit
-import com.zwsi.gblib.GBController.Companion.flyShipStarPatrol
-import com.zwsi.gblib.GBData
 import com.zwsi.gblib.GBData.Companion.BATTLESTAR
 import com.zwsi.gblib.GBData.Companion.CRUISER
 import com.zwsi.gblib.GBData.Companion.FACTORY
@@ -24,13 +22,6 @@ import com.zwsi.gblib.GBData.Companion.POD
 import com.zwsi.gblib.GBData.Companion.RESEARCH
 import com.zwsi.gblib.GBData.Companion.SHUTTLE
 import com.zwsi.gblib.GBData.Companion.STATION
-import com.zwsi.gblib.GBLocation
-import com.zwsi.gblib.GBLocation.Companion.DEEPSPACE
-import com.zwsi.gblib.GBLocation.Companion.LANDED
-import com.zwsi.gblib.GBLocation.Companion.ORBIT
-import com.zwsi.gblib.GBLocation.Companion.PATROL
-import com.zwsi.gblib.GBPatrolPoint
-import com.zwsi.gblib.distance
 
 
 class ShipFragment : Fragment() {
@@ -45,8 +36,6 @@ class ShipFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view: View = inflater.inflate(R.layout.fragment_ship, container, false)!!
-
-        setSpinner(view, this)
 
         setDetails(view)
 
@@ -65,8 +54,6 @@ class ShipFragment : Fragment() {
         val actionObserver = Observer<Int> { _ ->
             setDetails(view)
             view.invalidate()
-            val spinner = view.findViewById<Spinner>(R.id.spinner)
-
         }
         GBViewModel.actionsTaken.observe(this, actionObserver)
 
@@ -99,18 +86,18 @@ class ShipFragment : Fragment() {
             GlobalStuff.makeShip(it, STATION)
         })
 
-
         val zoomButton: Button = view.findViewById(R.id.panzoomToShip)
         zoomButton.tag = sh.uid
         zoomButton.setOnClickListener(View.OnClickListener {
-//            GlobalStuff.panzoomToShip(it)
+            GlobalStuff.panzoomToShip(it)
+        })
 
+        val destinationButton: Button = view.findViewById(R.id.destination)
+        destinationButton.setOnClickListener(View.OnClickListener {
             val intent = Intent(this.context, DestinationActivity::class.java)
             intent.putExtra("uidShip", sh.uid)
             startActivity(intent)
-
         })
-
 
         return view
     }
@@ -126,14 +113,17 @@ class ShipFragment : Fragment() {
         if (sh == null) {
             stats.setText("Boom! This ship no longer exists.")
         } else {
+
             stats.setText("Name: " + sh.name + "\n")
             stats.append("Type: " + sh.type + "\n")
-            stats.append("Speed: " + sh.speed + "\n")
-            stats.append("Health: " + sh.health + "\n")
-            stats.append("Weapons: " + sh.guns + "\n")
-            stats.append("Damage: " + sh.damage + "\n")
-            stats.append("Range: " + sh.range+ "\n")
             stats.append("Race: " + sh.race.name + "\n")
+            stats.append("Speed: " + sh.speed + "\n")
+            if (sh.guns > 0) {
+                stats.append("Weapons: " + sh.guns + " ")
+                stats.append("Damage: " + sh.damage + " ")
+                stats.append("Range: " + sh.range + "\n")
+            }
+            stats.append("Health: " + sh.health + "\n")
             stats.append("Location: " + sh.loc.getLocDesc() + "\n")
             if (sh.dest != null) stats.append("Destination: ${sh.dest?.getLocDesc()}\n")
 
@@ -146,6 +136,11 @@ class ShipFragment : Fragment() {
                 2 -> shipRaceView.setImageResource((R.drawable.beetle))
                 3 -> shipRaceView.setImageResource((R.drawable.tortoise))
             }
+
+            if (sh.uidRace != uidActivePlayer || sh.speed == 0) {
+                view.findViewById<Button>(R.id.destination).setVisibility(View.GONE)
+            }
+
 
             val shipView = view.findViewById<ImageView>(R.id.ShipView)
 
@@ -193,156 +188,6 @@ class ShipFragment : Fragment() {
                 shipView.setImageResource(R.drawable.yellow)
             }
 
-            val spinner = view.findViewById<Spinner>(R.id.spinner)
-            if (vm.secondPlayer && vm.playerTurns[uidActivePlayer] < GBViewModel.MIN_ACTIONS) {
-                spinner.isEnabled = false
-
-                // Hack to make spinner look disabled. getSelectedView.isEnabled=false didn't work
-                // We may replace this with a custom spinner, at which point there may be better options
-                spinner.alpha = 0.5f
-            } else {
-                spinner.isEnabled = true
-                spinner.alpha = 1f // See Hack above.
-            }
-        }
-    }
-
-    private fun setSpinner(view: View, fragment: ShipFragment) {
-
-        // FIXME Setting Destination after update/DO doesn't work
-
-        val sh = vm.ships[tag!!.toInt()] // Don't use ship() as we need to handle null (do nothing)
-
-        if (sh == null || sh.loc.level == DEEPSPACE) {
-            return
-        } else {
-            if (sh.uidRace != uidActivePlayer || sh.speed == 0) {
-                view.findViewById<Spinner>(R.id.spinner).setVisibility(View.INVISIBLE)
-            } else {
-                // TODO: better selection of possible targets once we have visibility.
-                // Right now it's all insystem and first planet of each system outside.
-                // TODO: If we have fly to a star system update the lists here
-                val destinationStrings = arrayListOf<String>()
-                val destinationUids =
-                    HashMap<String, Int>() // FIXME Use getItemAtPosition(position) and get it out of item.
-
-
-                var currentUidDestination = 1000
-
-                // Put current destination first, and avoid listing it again.
-                if (sh.dest != null) {
-
-                    val key: String
-
-                    when (sh.dest!!.level) {
-                        ORBIT, LANDED -> {
-                            key = "${sh.dest!!.getPlanet()!!.name} (current)"
-                            currentUidDestination = sh.dest!!.getPlanet()!!.uid
-                            destinationStrings.add(key)
-                            destinationUids[key] = currentUidDestination
-                        }
-                        PATROL -> {
-                            key = "${sh.dest!!.getPatrolPoint()!!.star.name} (current)"
-                            currentUidDestination = -sh.dest!!.getPatrolPoint()!!.star.uid
-                            destinationStrings.add(key)
-                            destinationUids[key] = currentUidDestination
-                        }
-                        else -> {
-                        }
-                    }
-                } else {
-                    val key = "Set Destination"
-                    destinationStrings.add(key)
-                }
-
-                val star = sh.getStar()
-                if (star != null) {
-                    for (p in star.starUidPlanets.map { vm.planet(it) }) {
-                        if (p.uid != currentUidDestination) {
-                            val key = "${p.name} (insystem)"
-                            destinationStrings.add(key)
-                            destinationUids[key] = p.uid
-                        }
-                    }
-                }
-                val sortedStars =
-                    vm.stars.toList().sortedBy { (_, s) ->
-                        s.loc.getLoc().distance(sh.loc.getLoc())
-                    }.take(6)
-
-                for ((_, star) in sortedStars) {
-                    if (star.uid != -currentUidDestination) {
-                        val key = "${star.name} system"
-                        destinationStrings.add(key)
-                        destinationUids[key] = -star.uid
-                    }
-                }
-
-                // Create an ArrayAdapter
-                val adapter = ArrayAdapter<String>(this.activity!!, R.layout.spinner_item, destinationStrings)
-
-                // Specify the layout to use when the list of choices appears
-                //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
-
-                // Apply the adapter to the spinner
-                val spinner = view.findViewById<Spinner>(R.id.spinner)
-                spinner.adapter = adapter
-                spinner.setSelection(1, false) // Hack ? To prevent being called twice
-
-                spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-
-                    override fun onNothingSelected(parent: AdapterView<*>?) {
-                    }
-
-                    override fun onItemSelected(parent: AdapterView<*>?, spinnerView: View?, position: Int, id: Long) {
-
-                        // Hack: onItemSelected is called twice. But the first time, spinnerView seems to be null
-                        if (spinnerView != null) {
-                            val destination = parent!!.getItemAtPosition(position).toString()
-
-                            if (destination == "Set Destination") { // FIXME make this a constant
-                                return
-                            }
-
-                            var uid = destinationUids[destination]!!
-
-                            if (uid > 0) { // Planet
-                                val planet = vm.planet(uid)
-
-                                if (sh.idxtype == GBData.POD) {
-                                    flyShipLanded(sh.uid, planet.uid) // update server side
-                                    sh.dest = GBLocation(planet, 0, 0) // update vm
-                                } else {
-                                    flyShipPlanetOrbit(sh.uid, planet.uid)// update server side
-                                    sh.dest = GBLocation(planet, GBData.PlanetOrbit, 0f) // update vm
-                                }
-                            } else { // Star
-                                uid = -uid
-                                val star = vm.star(uid)
-                                val patrolPoint: GBPatrolPoint
-                                if (GBData.rand.nextBoolean()) {
-                                    patrolPoint = vm.patrolPoint(star.starUidPatrolPoints.first())
-                                } else {
-                                    patrolPoint = vm.patrolPoint(star.starUidPatrolPoints.drop(1).first())
-                                }
-                                flyShipStarPatrol(sh.uid, patrolPoint.uid)// update server side
-                                sh.dest = GBLocation(patrolPoint, 0f, 0f) // update vm // FIXME select patrol point
-                            }
-                            GlobalStuff.checkDo(view)
-
-                            Toast.makeText(
-                                view.context,
-                                "Ordered " + sh.name + " to fly to " + sh.dest,
-                                Toast.LENGTH_SHORT
-                            )
-                                .show()
-
-                            fragment.setDetails(view)
-                        }
-                    }
-                }
-            }
         }
     }
 }
