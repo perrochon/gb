8de0520ee9979e6ef3942d2598b0d83e6539d16a
Louis Perrochon <perrochon@gmail.com>
2020-04-07 11:47:51 -0700

    No longer use FindViewByID (but use Kotlin plugin instead) Also now using onViewCreated in addition to onCreateView, which is a) correct and makes the code easier and b) makes the plugin work

--- app/src/main/java/com/zwsi/ar/app/ARStarFragment.kt
+++ app/src/main/java/com/zwsi/ar/app/ARStarFragment.kt
@@ -6,63 +6,56 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import com.zwsi.ar.app.ARViewModel.Companion.superSensors
 import com.zwsi.ar.app.ARViewModel.Companion.uidActivePlayer
 import com.zwsi.ar.app.ARViewModel.Companion.vm
-
+import kotlinx.android.synthetic.main.fragment_star.*
 
 class ARStarFragment : Fragment() {
 
     companion object {
 
-        fun newInstance(@Suppress("UNUSED_PARAMETER")message: String): ARStarFragment {
+        fun newInstance(@Suppress("UNUSED_PARAMETER") message: String): ARStarFragment {
             return ARStarFragment()
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_star, container, false)!!
+    }
 
-        val view: View = inflater.inflate(R.layout.fragment_star, container, false)!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        setDetails(view)
+        setDetails()
 
-        val turnObserver = Observer<Int> { _->
-            setDetails(view)
+        val turnObserver = Observer<Int> { _ ->
+            setDetails()
             view.invalidate()
         }  // TODO why is newTurn nullable?
         ARViewModel.currentTurn.observe(this, turnObserver)
 
-        val starButton: Button = view.findViewById(R.id.panzoomToStar)
-        starButton.tag = tag!!.toInt()
-        starButton.setOnClickListener(View.OnClickListener {
+        button_to_star.tag = tag!!.toInt()
+        button_to_star.setOnClickListener(View.OnClickListener {
             GlobalStuff.panzoomToStar(it)
         })
-
-
-        return view
     }
 
-    private fun setDetails(view: View) {
+    private fun setDetails() {
 
         // Stars don't go away, so the below !! should be safe
         val star = vm.star(tag!!.toInt())
 
-        val stats = view.findViewById<TextView>(R.id.StarStats)
-        val paint = stats.paint
-        paint.textSize = 40f
-
-        stats.text="${star.name} at (" + (star.loc.getLoc().x.toInt()) + ", " + star.loc.getLoc().y.toInt() + ")\n"
+        StarStats.text =
+            "${star.name} at (" + (star.loc.getLoc().x.toInt()) + ", " + star.loc.getLoc().y.toInt() + ")\n"
 
         if (superSensors || vm.race(uidActivePlayer).raceVisibleStars.contains(star.uid)) {
             val planets = star.starUidPlanets.map { vm.planet(it) }
             if (planets.isNotEmpty()) {
-                stats.append("Planets (${planets.size}): ")
+                StarStats.append("Planets (${planets.size}): ")
                 for (p in planets) {
-                    stats.append("  " + p.name + " ")
+                    StarStats.append("  " + p.name + " ")
                 }
-                stats.append("\n")
+                StarStats.append("\n")
             }
 
             val uidShips = star.starUidShips
@@ -70,9 +63,9 @@ class ARStarFragment : Fragment() {
                 uidShips += vm.patrolPoint(uidPP).orbitUidShips
             }
             if (uidShips.isNotEmpty()) {
-                stats.append("Ships (${uidShips.size.toString()}): ")
+                StarStats.append("Ships (${uidShips.size.toString()}): ")
                 for (s in uidShips) {
-                    stats.append(vm.ship(s).name + " ")
+                    StarStats.append(vm.ship(s).name + " ")
                 }
             }
         }
